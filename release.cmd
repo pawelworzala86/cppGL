8cb8102e9e9ce9e632ef83795b650b426cc9d5f3
pawelworzala86 <pawelworzala86@gmail.com>
2024-05-31 18:08:47 +0200

    ref

--- release.cmd
+++ release.cmd
@@ -1,4 +1,4 @@
-g++.exe -Wall -O2  -c C:\cppGL\main.cpp -o obj\Release\main.o
+g++.exe -Wall -O2 -s -c C:\cppGL\main.cpp -o obj\Release\main.o
 g++.exe  -o bin\Release\cppGL.exe obj\Release\main.o  -s -LC:\cppGL\ -lopengl32 -lglu32 -lgdi32 -mwindows -lfreeimage
 
 cd dist
